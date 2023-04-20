7eb24bfeff91e3f6aaa68886475cdeb3cff76259
Jakka <h.jakka@dxc.com>
2023-04-20 12:24:19 +0530

    updated

---
+++ csharp/Largest_number/Largest_number/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Largest_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter 1st number");
+            int num1=Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("enter 2nd number");
+            int num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("enter 3rd number");
+            int num3 = Convert.ToInt16(Console.ReadLine());
+            if((num1>num2) && (num1>num3)) {
+                Console.WriteLine("num1 is greater");
+            
+            }
+            else if(num2>num3) 
+            {
+                Console.WriteLine("num2 is greater");
+            }
+            else
+            {
+                Console.WriteLine("num3 is greater");
+            }
+
+        }
+    }
+}
---
+++ csharp/vowel/vowel/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Design;
+
+namespace vowel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a character");
+            char ch1=Convert.ToChar(Console.ReadLine());
+            if (ch1 == 'a' || ch1 == 'A' || ch1 == 'e' || ch1 == 'E' || ch1 == 'i' || ch1 == 'I' || ch1 == 'o' || ch1 == 'O' || ch1 == 'u' || ch1 == 'U')
+            {
+                Console.WriteLine("ch1 is a vowel");
+            }
+            else
+                Console.WriteLine("ch1 is not a vowel");
+
+            
+        }
+    }
+}
