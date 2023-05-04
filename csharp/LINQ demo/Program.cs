e5518eeb9f9f369452d21cbbe5f06fd04be1ab4c
Jakka <h.jakka@dxc.com>
2023-05-04 12:19:48 +0530

    updated

---
+++ csharp/LINQ demo/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LINQ_demo
+{
+    class product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string brand { get; set; }
+        public int Quantity { get; set; }
+        public int price { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<product> products = new List<product>()
+            {
+                new product { Id = 1,Name="laptop",brand="dell",Quantity=2,price=20000},
+                new product { Id = 2,Name="camera",brand="canon",Quantity=1,price=10000},
+                new product { Id = 3,Name="tv",brand="lg",Quantity=2,price=50000},
+
+            };
+            var query=from p in products where p.Id == 1 select p;
+            foreach(var p in query)
+            {
+                Console.WriteLine($"{p.Id} {p.Name} {p.brand} {p.Quantity} {p.price}");
+            }
+            var result=products.Where(x=>x.Id == 2).ToList();
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Id} {item.Name} {item.brand} {item.Quantity} {item.price}");
+            }
+            var ans=products.Find(x=>x.Name == "tv");
+            Console.WriteLine($"{ans.Id} {ans.Name} {ans.brand} {ans.Quantity} {ans.price}");
+        }
+    }
+}
---
+++ csharp/sortdemo/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace sortdemo
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public int Quantity  { get; set; }
+        public int Price { get; set; }
+    }
+    class SortByQuantity : IComparer<Product>
+    {
+        public int Compare(Product? x, Product? y)
+        {
+            if (x.Quantity > y.Quantity)
+            {
+                return 1;
+            }
+            else if (x.Quantity < y.Quantity) { return -1; }
+            else { return 0; }
+        }
+    }
+    class SortByPrice : IComparer<Product>
+    {
+        public int Compare(Product? x, Product? y)
+        {
+            if(x.Price>y.Price)
+            {
+                return 1;
+            }
+            else if(x.Price < y.Price)
+            {
+                return -1;
+            }
+            else { return 0; }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product { Id = 1, Name="Laptop", Brand="Dell", Quantity=5, Price=46500},
+                new Product { Id = 2, Name="Camera", Brand="Canon", Quantity=7, Price=32300},
+                new Product { Id = 3, Name="Tablet", Brand="Lenovo", Quantity=2, Price=18500},
+                new Product { Id = 4, Name="Desktop", Brand="HP", Quantity=9, Price=78200},
+                new Product { Id = 5, Name="Earphones", Brand="Boat", Quantity=3, Price=3000},
+                new Product { Id = 6, Name="Mobile", Brand="Apple", Quantity=4, Price=84000},
+            };
+            products.Sort(new SortByPrice());
+                foreach(Product p in products)
+                {
+                    Console.WriteLine($"{p.Id} {p.Name} {p.Brand} {p.Quantity} {p.Price}");
+                }
+            Console.WriteLine("sorting through quantity");
+            products.Sort(new SortByQuantity());
+            foreach(Product p in products)
+            {
+                Console.WriteLine($"{p.Id} {p.Name} {p.Brand} {p.Quantity} {p.Price}");
+            }
+            
+
+        }
+    }
+}
